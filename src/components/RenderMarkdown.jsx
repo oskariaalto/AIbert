1723795bcac0013f0cfc78d821f0356da1479ef5
Oskari Aalto <heikki.oskari.aalto@gmail.com>
2023-11-29 17:51:57 +0200

    added hints

--- src/components/RenderMarkdown.jsx
+++ src/components/RenderMarkdown.jsx
@@ -2,10 +2,16 @@ import { MathComponent } from "mathjax-react";
 import PropTypes from "prop-types";
 
 const LatexRenderer = ({ value }) => {
-    //const parts  = value.split('$');
+    const parts  = value.split('$');
     return (
       <div>
-        <MathComponent  tex={value} />;
+        {parts.map((part, index) => {
+          if (index % 2 === 0) {
+            return <span key={index}>{part}</span>;
+          } else {
+            return <MathComponent key={index} tex={part} />;
+          }
+        })}
       </div>
     );
 };
